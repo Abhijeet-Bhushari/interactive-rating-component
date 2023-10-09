d678e618afb85efb03a8d1bd7998eab6b813ba08
Abhijeet-Bhushari <abhijeetvijaybhushari242002@gmail.com>
2023-10-09 22:51:52 +0530

    added submit rating component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Rating from './components/Rating/Rating'
+import SubmitRating from './components/Rating/SubmitRating'
 function App() {
   return (
     <div className="App">
-      <Rating/>
+      <SubmitRating/>
     </div>
   );
 }
